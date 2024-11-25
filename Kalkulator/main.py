08497b2dc28305694f86ec13f17ad657fc5705dd
madastt <139136278+madastt@users.noreply.github.com>
2024-11-25 16:28:59 +0100

    Add files via upload

--- Kalkulator/main.py
+++ Kalkulator/main.py
@@ -1,40 +1,58 @@
 print("Witam w kalkulatorze :-)")
 while True:
-    option = int(input("Wybierz opcję:\n1. Dodawanie\n2. Odejmowanie \n3. Mnożenie\n4. Dzielenie\n"
-                       "5. Pierwiastkowanie\n6. Potęgowanie\n7. Procent z liczby\n8. Koniec\n>> "))
+    try:
+        option = int(input("Wybierz opcję:\n1. Dodawanie\n2. Odejmowanie \n3. Mnożenie\n4. Dzielenie\n"
+                           "5. Pierwiastkowanie\n6. Potęgowanie\n7. Procent z liczby\n8. Koniec\n>> "))
 
-    if option == 8:
-        print("Dziękujemy za skorzystanie z naszego kalkulatora :-)")
-        break
-    if option < 5:
-        def calculator(optioncalc):
-            number1 = int(input("Podaj pierwszą liczbę:\n>>  "))
-            number2 = int(input("Podaj drugą liczbę\n>> "))
-            if optioncalc == 1:
-                answercalc = number1 + number2
-            elif optioncalc == 2:
-                answercalc = number1 - number2
-            elif optioncalc == 3:
-                answercalc = number1 * number2
-            elif optioncalc == 4:
-                answercalc = number1 / number2
-            return answercalc
+        # Sprawdzenie, czy użytkownik chce zakończyć działanie kalkulatora
+        if option == 8:
+            print("Dziękujemy za skorzystanie z naszego kalkulatora :-)")
+            break
 
-        answer = calculator(option)
+        # Funkcja pomocnicza do pobierania liczby
+        def get_number(prompt):
+            return float(input(prompt))
 
-    elif option == 5:
-        number1 = int(input("Z jakiej liczby chcesz pierwiastek:\n>> "))
-        number2 = int(input("Jakiego stopnia:\n>> "))
-        answer = pow(number1, 1 / number2)
+        # Podstawowe operacje arytmetyczne
+        if option in [1, 2, 3, 4]:
+            number1 = get_number("Podaj pierwszą liczbę:\n>>  ")
+            number2 = get_number("Podaj drugą liczbę\n>> ")
 
-    elif option == 6:
-        number1 = int(input("Podaj liczbę do zpotęgowania:\n>> "))
-        number2 = int(input("Podaj stopień potęgi:\n>> "))
-        answer = pow(number1, number2)
+            if option == 1:
+                answer = number1 + number2
+            elif option == 2:
+                answer = number1 - number2
+            elif option == 3:
+                answer = number1 * number2
+            elif option == 4:
+                if number2 == 0:
+                    print("Błąd: Nie można dzielić przez zero!")
+                    continue
+                answer = number1 / number2
 
-    elif option == 7:
-        number1 = int(input("Podaj liczbę:\n>> "))
-        number2 = int(input("Jaki procent chcesz z tej liczby:\n>> "))
-        answer = number1*number2/100
+        # Pierwiastkowanie
+        elif option == 5:
+            number1 = int(input("Z jakiej liczby chcesz pierwiastek:\n>> "))
+            number2 = int(input("Jakiego stopnia:\n>> "))
+            answer = pow(number1, 1 / number2)
 
-    print("Wynik: ", answer)
+        # Potęgowanie
+        elif option == 6:
+            number1 = int(input("Podaj liczbę do zpotęgowania:\n>> "))
+            number2 = int(input("Podaj stopień potęgi:\n>> "))
+            answer = pow(number1, number2)
+
+        # Obliczanie procentu z liczby
+        elif option == 7:
+            number1 = int(input("Podaj liczbę:\n>> "))
+            number2 = int(input("Jaki procent chcesz z tej liczby:\n>> "))
+            answer = number1*number2/100
+
+        else:
+            print("Nieprawidłowa opcja, wybierz ponownie.")
+            continue
+
+        print("Wynik: ", answer)
+
+    except ValueError:
+        print("Błąd: Wprowadzono nieprawidłową wartość. Spróboj ponownie")
